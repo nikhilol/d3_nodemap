cbddf998600439c3cf3fc33154de68c5ae6787d4
nikhilol <nikhil.patel.np@outlook.com>
2021-07-08 10:30:17 +0100

    Added completion of nodes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import { Graph } from 'react-d3-graph'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import { Button } from '@material-ui/core'
 import './App.css';
 
-import MarkdownIt from 'markdown-it'
-import marked from 'marked'
-import MdEditor from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css';
 import Editor from "rich-markdown-editor"
 
 const axios = require('axios')
-const d3 = require("d3");
-
+const marked = require('marked')
 
 const myConfig = {
   "automaticRearrangeAfterDropNode": false,
@@ -20,7 +17,7 @@ const myConfig = {
   "focusAnimationDuration": 0.75,
   "focusZoom": 1,
   "freezeAllDragEvents": false,
-  "height": window.innerHeight - 10,
+  "height": window.innerHeight * 0.94,
   "highlightDegree": 1,
   "highlightOpacity": 1,
   "linkHighlightBehavior": false,
@@ -30,7 +27,7 @@ const myConfig = {
   "panAndZoom": false,
   "staticGraph": false,
   "staticGraphWithDragAndDrop": true,
-  "width": window.innerWidth * 0.7,
+  "width": window.innerWidth * 0.6,
   "d3": {
     "alphaTarget": 0.05,
     "gravity": -100,
@@ -80,12 +77,12 @@ const myConfig = {
 
 function App() {
 
-  const [data, setData] = useState(null)
+  const [data, setData] = useState({})
   const [activeNode, setActiveNode] = useState(null)
-  const [mdValue, setMdValue] = useState('')
+  const [mdValue, setMdValue] = useState([])
 
   function getData() {
-    return axios.get('https://us-central1-nodemap-app.cloudfunctions.net/api/plan/nodes?username=Demo&plan_id=Test').then(_d => _d.data)
+    return axios.get(`https://us-central1-nodemap-app.cloudfunctions.net/api/plan/nodes?username=Demo&plan_id=Test plan`).then(_d => _d.data)
   }
 
   useEffect(() => {
@@ -101,12 +98,14 @@ function App() {
           data[key].id = data[key].ID;
           delete data[key].ID;
 
-          data[key].x = data[key].x ? data[key].x :  100;
-          data[key].y = data[key].y ? data[key].y : parseInt(key) * 150
+          data[key].x = data[key].x ? data[key].x : window.innerWidth * 0.3;
+          data[key].y = data[key].y ? data[key].y : window.innerHeight * 0.1 + parseInt(key) * 150
 
           data[key].svg = `/Logos/${data[key].Platform}`
           if (!data[key].md) {
-            data[key].md = `## ${data[key].Title} ${data[key].Platform} ##`
+            data[key].md = `### ${data[key].Platform} ### \n # ${data[key].Title} # \n --- `
+          } else {
+
           }
           if (key > 0) {
             links.push({ source: data[key - 1].id, target: data[key].id, color: data[key].IsComplete ? '#72EFDD' : '#D2D2D2' })
@@ -122,59 +121,84 @@ function App() {
     return () => mounted = false;
   }, [])
 
-  const onClickNode = function (nodeId, node) {
+  async function configureLinks() {
+    let temp = data;
+    temp.links = []
+    for (var key in temp.nodes) {
+      if (key > 0) {
+        temp.links.push({ source: temp.nodes[key - 1].id, target: temp.nodes[key].id, color: temp.nodes[key].IsComplete ? '#72EFDD' : '#D2D2D2' })
+      }
+    }
+    await setData({ nodes: temp.nodes, links: temp.links })
+  }
+
+  const onDblClickNode = function (nodeId, node) {
     setActiveNode(node)
     setMdValue(node.md)
     console.log(node)
   };
 
-  const handleEditorChange = ({ html, text }) => {
-    const newValue = text.replace(/\d/g, "");
+  function handleEditorChange(getText) {
+    const newValue = getText();
     console.log(newValue);
-    setMdValue(newValue);
-    updateNodeData(activeNode.id, 'md', newValue)
-    activeNode.md = newValue
+    // setMdValue(newValue);
+    if (activeNode) {
+      updateNodeData(activeNode ? activeNode.id : null, 'md', newValue)
+      activeNode.md = newValue
+    }
   }
 
   function updateNodeData(nodeId, property, newValue) {
     let temp = data
     for (let key in temp.nodes) {
       if (temp.nodes[key].id === nodeId) {
-        temp.nodes[key][property] = newValue
+        temp.nodes[key][property] = newValue === 'BOOL' ? !temp.nodes[key][property] : newValue
+        console.log(`Updating ${nodeId}'s property ${property} to ${newValue}`)
       }
     }
     setData(temp)
+    console.log(temp)
   };
 
-  function onNodePositionChange(node){
-    updateNodeData(node.id, 'fx', node.x)
+  function onNodePositionChange(node) {
+    // updateNodeData(node, 'fx', node.x)
+  }
+
+  function onClickNode(node) {
+    console.log(node)
+    updateNodeData(node, 'IsComplete', 'BOOL')
+    configureLinks()
   }
 
   return (
-    <div style={{ display: 'flex', height: '100vh', background:'#F7F6F2' }} className="App">
-      {data &&
-        <>
-          <div style={{height:'100vh'}}>
-            <MdEditor
-              style={{ height: "100%", width: '100%' }}
-              renderHTML={(text) => marked(text)}
-              onChange={handleEditorChange}
-              value={mdValue}
-            />
-
-          </div>
-          <div style={{ width: '60%' }}>
-            <Graph
-              id="graph_id"
-              data={data}
-              config={myConfig}
-              onClickNode = {onClickNode}
-              onNodePositionChange = {onNodePositionChange}
-            />
-          </div>
-        </>
-      }
-    </div >
+    <>
+      <nav style={{ height: '5vh', background: '#2b2b2b' }}></nav>
+      <div style={{ display: 'flex', height: 'auto', background: '#F7F6F2' }} className="App">
+        {data &&
+          <>
+            <div style={{ height: '95vh', position: 'relative' }}>
+              <Editor
+                style={{ minWidth: '30vw', maxWidth: '30vw', minHeight: '100%', maxHeight: '100%', textAlign: 'left', background: '#FFF', borderRight: '1px solid #d2d3d4' }}
+                value={activeNode ? activeNode.md : "t"}
+                defaultValue={activeNode ? activeNode.md : ""}
+                onChange={handleEditorChange}>
+              </Editor>
+            </div>
+            <div style={{ width: '60vw', minWidth: '60vw' }}>
+              <Graph
+                id="graph_id"
+                data={data}
+                config={myConfig}
+                onClickNode={onClickNode}
+                onNodePositionChange={onNodePositionChange}
+                onMouseOverNode={onDblClickNode}
+              />
+              <Button>TEST</Button>
+            </div>
+          </>
+        }
+      </div >
+    </>
   );
 }
 
