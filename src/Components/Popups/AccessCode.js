6f79333634f7cb0dccf35b5a72cbf053082572db
Nikhil <nikhil.patel.np@outlook.com>
2021-09-16 07:11:17 +0100

    notes and acessCode file

---
+++ src/Components/Popups/AccessCode.js
@@ -0,0 +1,10 @@
+import { Dialog } from '@material-ui/core'
+import React from 'react'
+
+export default function AccessCode(){
+    return(
+        <Dialog open>
+            PLEASE ENTER ACCESS CODE
+        </Dialog>
+    )
+}
