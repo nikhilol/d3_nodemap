dde1eb0777e551ddbac0775648474d6560a6dd9c
Nikhil <nikhil.patel.np@outlook.com>
2021-10-26 08:49:39 +0100

    Actual fix

--- functions/Automations/Notion/CreateBrainstormSpaceDaily.js
+++ functions/Automations/Notion/CreateBrainstormSpaceDaily.js
@@ -6,7 +6,7 @@ const functions = require("firebase-functions");
 const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: "secret_qFHmY8UZ0H1RsexDyedFHicyeSZ41Q0CVYnL9qvmtGp" });
 
-exports.createBrainstormSpaceDaily = functions.pubsub.schedule("35 8 * * *").timeZone("Europe/London").onRun((context) => {
+exports.createBrainstormSpaceDaily = functions.pubsub.schedule("1 0 * * *").timeZone("Europe/London").onRun((context) => {
     const blockId = "103af17d47e647a3bc9b5aa3c6e7b24e";
     const date = new Date().toLocaleDateString("en-GB");
     notion.blocks.children.append({
